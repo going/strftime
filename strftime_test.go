36754a2ab574a8f81776d60a7490ebada6b0d861
Tony.Shao <xiocode@gmail.com>
2015-05-04 14:34:21 +0800

    strftime

---
+++ strftime_test.go
@@ -0,0 +1,23 @@
+/**
+ * Author:        Tony.Shao
+ * Email:         xiocode@gmail.com
+ * Github:        github.com/xiocode
+ * File:          strftime_test.go
+ * Description:   test
+**/
+
+package strftime
+
+import (
+	"testing"
+	"time"
+)
+
+func TestFormat(t *testing.T) {
+	t.Log(Format("%Y-%m-%d %H:%M:%S", time.Now()))
+}
+
+func TestParse(t *testing.T) {
+	t.Log(Parse("%Y-%m-%d %H:%M:%S", "2013-04-15 02:10:06"))
+	t.Log(Parse("%d/%b/%Y:%H:%M:%S %z", "14/Apr/2013:18:38:28 +0800"))
+}
